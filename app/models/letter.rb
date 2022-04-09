53f63de9df0b125b2bb60e693e2b1daebd1cf935
Trevor Broaddus <trevor.broaddus@hey.com>
2022-04-09 07:51:40 -0500

    Create links to PDF documents in search results

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,7 +1,12 @@
 class Letter < ApplicationRecord
-  BASE_PATH = "public/letters"
+  SUBDIRECTORY = "letters"
+  BASE_PATH = "public/#{SUBDIRECTORY}"
 
   def filepath
     @filepath ||= Rails.root.join(BASE_PATH, filename)
+  end
+
+  def linkpath
+    @linkpath ||= "/#{SUBDIRECTORY}/#{filename}"
   end
 end
