75614b597248f531a60a4261a6305cc6a8ff84f0
Trevor Broaddus <42982667+tangentus@users.noreply.github.com>
2022-06-04 14:57:40 -0500

    Add QR codes and utilize asset pipeline for letters (#9)

--- app/components/letter_component.rb
+++ app/components/letter_component.rb
@@ -1,9 +1,28 @@
 # frozen_string_literal: true
 
 class LetterComponent < ViewComponent::Base
-  attr_reader :letter
+  attr_reader :letter, :letter_path, :letter_url
 
   def initialize(letter:)
     @letter = letter
+  end
+
+  def letter_path
+    @letter_path ||= asset_path(letter.filename)
+  end
+
+  def letter_url
+    @letter_url ||= asset_url(letter.filename)
+  end
+
+  def qr_code_svg
+    qr = RQRCode::QRCode.new(letter_url)
+    qr.as_svg(
+      color: "fff",
+      shape_rendering: "crispEdges",
+      module_size: 5,
+      standalone: true,
+      use_path: true
+    )
   end
 end
--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,12 +1,2 @@
 class Letter < ApplicationRecord
-  SUBDIRECTORY = "letters"
-  BASE_PATH = "public/#{SUBDIRECTORY}"
-
-  def filepath
-    @filepath ||= Rails.root.join(BASE_PATH, filename)
-  end
-
-  def linkpath
-    @linkpath ||= "/#{SUBDIRECTORY}/#{filename}"
-  end
 end
