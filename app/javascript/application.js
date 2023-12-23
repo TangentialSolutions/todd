d00589e539fb1afb759d37c02f75cbb489459813
Trevor Broaddus <trevor.broaddus@hey.com>
2023-12-23 00:43:06 -0600

    Install web5 api

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import { Web5 } from "@web5/api";
+const { web5, did: myDid } = await Web5.connect();
+
+console.log(myDid);
